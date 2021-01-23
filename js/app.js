603849171e1ad086c8b5d779dc0b870f00aabb48
Jorge Calderon <cpjo168599@upemor.edu.mx>
2021-01-23 17:36:47 -0600

    Portafolio

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const menuHamburgesa = document.querySelector('.fa-bars');
+const menu = document.querySelector('.enlaces-header');
+
+menuHamburgesa.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+window.addEventListener('click', e => {
+  if (menu.classList.contains('active') && e.target !== menuHamburgesa && e.target !== menu && e.target !== document.querySelector('.carousel__next')) {
+    menu.classList.toggle('active');
+
+  }
+});
+
+window.addEventListener('resize', () => {
+  comprobarAncho();
+});
+
+const comprobarAncho = () => {
+  if (window.innerWidth <= 1000) {
+    lightbox.option({
+      'resizeDuration': 500,
+      'wrapAround': true,
+      'albumLabel': "Imagen %1 de %2",
+      'positionFromTop': 135,
+      'fitImagesInViewport': true,
+    });
+  }
+}
+
+comprobarAncho();
+
+lightbox.option({
+  'alwaysShowNavOnTouchDevices': true,
+  'resizeDuration': 500,
+  'wrapAround': true,
+  'albumLabel': "Imagen %1 de %2",
+  'positionFromTop': 40,
+  'fitImagesInViewport': false,
+  'maxWidth': 1000
+});
